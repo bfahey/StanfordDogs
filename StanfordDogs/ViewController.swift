8f4265cadf191823064db626972a6f4f48d667af
Blaine Fahey <bfahey@users.noreply.github.com>
2019-11-18 22:36:41 -0600

    Initial Commit

---
+++ StanfordDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StanfordDogs
+//
+//  Created by Blaine Fahey on 11/18/19.
+//  Copyright © 2019 Blaine Fahey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
